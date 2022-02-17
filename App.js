2f933387f552cd3967e4cc5a8d932b4083439e7b
Carlos <capz2003@gmail.com>
2022-02-17 17:09:51 -0500

    First commit

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*todays taks*/}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View styles={styles.items}>{/*tasks goe here*/}
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -14,7 +19,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items:  {}
 });
---
+++ components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, text, StyleSheet} from 'react-native'
+
+export const Task = () => {
+  return (
+    <View>
+        <Text>This is a task</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Task
